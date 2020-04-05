c4458c08b90d7920e61934b987b7bf77ad9c1a58
Amrit Mohan <yw7d5e63ir8e@users.noreply.github.com>
2020-04-05 14:46:06 +0200

    adding the .js file

---
+++ docs/covid19_map_data.js
@@ -0,0 +1,70 @@
+eqfeed_callback({
+  "operationStatus": "SUCCESS",
+  "operationResponse": [
+    {
+      "value": {
+        "longitude": 11.9817633,
+        "symptom-Moderate-to-severe-cough": true,
+        "symptom-Sore-throat": true,
+        "latitude": 57.6958466,
+        "diagnosis-Flu-Symptoms": true,
+        "symptom-High-fever": true,
+        "timestamp": 1586008393886,
+        "diagnoses": [
+          "Flu-Symptoms"
+        ],
+        "symptoms": [
+          "Moderate-to-severe-cough",
+          "Sore-throat",
+          "High-fever"
+        ]
+      },
+      "submitter": {
+        "identifier": "817643a0-3f3e-415d-b58b-13b0be551807",
+        "generatedBy": "ANONYMOUS_USER",
+        "details": {}
+      },
+      "modificationDate": "04/Apr/2020:13:53:15 ",
+      "submissionDate": "04/Apr/2020:13:53:15 ",
+      "uuid": "01200719-bcc9-483c-b6fc-fb89f34ca264"
+    },
+    {
+      "submissionDate": "04/Apr/2020:13:32:15 ",
+      "value": {
+        "longitude": 11.992435,
+        "diagnosis-Covid19-Positive": true,
+        "latitude": 57.6952483,
+        "timestamp": 1586007132483,
+        "diagnoses": [
+          "Covid19-Positive"
+        ],
+        "symptoms": []
+      },
+      "submitter": {
+        "generatedBy": "ANONYMOUS_USER",
+        "details": {},
+        "identifier": "56389f09-9540-41ee-9a21-791ebdd132b7"
+      },
+      "uuid": "3343f737-1299-4e71-b03b-4880de87d75c",
+      "modificationDate": "04/Apr/2020:13:32:15 "
+    },
+    {
+      "submissionDate": "04/Apr/2020:14:33:00 ",
+      "value": {
+        "longitude": 11.9820582,
+        "latitude": 57.6960678,
+        "timestamp": 1586010779054,
+        "diagnoses": [],
+        "symptoms": []
+      },
+      "submitter": {
+        "generatedBy": "ANONYMOUS_USER",
+        "details": {},
+        "identifier": "817643a0-3f3e-415d-b58b-13b0be551807"
+      },
+      "uuid": "89b5ed46-94b3-49eb-83d9-609423b8c804",
+      "modificationDate": "04/Apr/2020:14:33:00 "
+    }
+  ],
+  "reponseCount": 3
+});
